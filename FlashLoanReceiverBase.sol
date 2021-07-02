b1e0ccf15aafb0abf704f4371ada7de11f7e74fd
merchant862 <43372874+merchant862@users.noreply.github.com>
2021-07-02 21:04:32 +0500

    Update FlashLoanReceiverBase.sol

--- FlashLoanReceiverBase.sol
+++ FlashLoanReceiverBase.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.6.6;
 
 import "./openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
-import "./openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/merchant862/flash-arb-aave/blob/master/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "./openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./IFlashLoanReceiver.sol";
 import "./ILendingPoolAddressesProvider.sol";
